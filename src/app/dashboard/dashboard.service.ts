43876abba8184f8033fe8d3f7c251735a540d264
swapna challa <swapnachalla@gmail.com>
2020-04-07 04:58:50 +0000

    first commit

---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  constructor(private _http: HttpClient) { 
+    const endpoint = 'http://localhost:8080/dashboard/v1/';
+    const httpOptions = {
+    headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+ };
+
+
+  }
+  
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+  
+}

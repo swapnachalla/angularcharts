95b842bafe0161b9664eb2e436900cea70a9f17a
Swapna Challa <swapnachalla@gmail.com>
2020-04-07 01:41:45 -0400

    Update my-bar-chart.component.ts

--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -12,13 +12,14 @@ export class MyBarChartComponent implements OnInit {
     responsive: true
   };
 
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July'];
   public barChartType = 'bar';
   public barChartLegend = true;
 
   public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Project A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Project B'},
+    {data: [40, 70, 10, 50, 15, 87, 50], label: 'Project C'}
   ];
 
   constructor() { }
